6a7bfae0f2f34a95e11d3e60d7f37c62fe9db88d
airpass <cwme12@airpass.co.kr>
2023-12-14 13:45:25 +0900

    feat:업데이트,삭제 쿼리 예제 기능 추가

--- index.js
+++ index.js
@@ -81,13 +81,13 @@ generatePresignedUrl(bucketName,key);
 
 
 ///////////////////////API 작업 with 시퀄라이즈(ORM) ///////////////////////////////////////////////////////
-// 특정 컨텐츠 조회 with XR_CNTNT_RS
-app.get('/api/getContInfo/:idx', async (req, res) => {
+
+// 콘텐츠 리스트 전체 with XR_CNTNT_RS
+app.get('/api/getContList', async (req, res) => {
     try {
-        const idx = (req.params.idx)? req.params.idx : 134;
-        const contInfo = await cntntInf.findOne({
+        const contList = await cntntInf.findAll({
             where:{
-                CNTI_IDX: idx,
+                CNTI_USE: 'Y',
             },
             include: [  // 모델 관계 설정한 데이터 같이 가져오기
                 {
@@ -96,41 +96,109 @@ app.get('/api/getContInfo/:idx', async (req, res) => {
                 },
             ],
         });
-        res.json(contInfo);
+        res.json(contList);
+        await sequelize.close();
     } catch (error) {
         console.error('Error fetching users:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
+
+// 특정 컨텐츠 조회 with XR_CNTNT_RS
+app.get('/api/getContInfo/:idx', async (req, res) => {
+        try {
+            const idx = (req.params.idx) ? req.params.idx : 134;
+            const contInfo = await cntntInf.findOne({
+                where: {
+                    CNTI_IDX: idx,
+                },
+                include: [  // 모델 관계 설정한 데이터 같이 가져오기
+                    {
+                        model: RS,
+                        as: 'rs' // 배열 키값
+                    },
+                ],
+            });
+            res.json(contInfo);
+            await sequelize.close();
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+});
+
 // 콘텐츠 저장 POST 방식으로 데이터 받아서 해야하나 임시로
 app.get('/api/insertCont',async (req, res) => {
-    try {
-        // 테스트 내역 저장
-        const newCont = await cntntInf.create({ 
-            CNTI_CODE: 'XR.10.testesttest11',
-            CNTT_CODE: 'XR',
-            CNTC_CODE: '30',
-            SS_CODE: '22',
-            CRT_CODE: 20,
-            CNTI_REQUIRE_SENSOR:'00,10',
-            CNTI_NAME: '2222222',
-            CNTI_E_NAME: '2222222',
-            CNTI_VERSION: '1.1',
-            CNTI_F_NAME: '2222222',
-            CNTI_F_ID: '2222222',
-            CNTI_F_PATH: '/uploads/contents/XR.10.testtesttest',
-            CNTI_MIN_UCNT: 1,
-            CNTI_MAX_UCNT: 2,
-            CNTI_DESC: '인설트 테스트',
-            CNTI_SORT:161,
-            CNTI_SORTBY: 'DESC',
-        });
-    } catch (error) {
-        console.error('Error fetching users:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+        try {
+            // 테스트 내역 저장
+            const newCont = await cntntInf.create({
+                CNTI_CODE: 'XR.10.testesttest11',
+                CNTT_CODE: 'XR',
+                CNTC_CODE: '30',
+                SS_CODE: '22',
+                CRT_CODE: 20,
+                CNTI_REQUIRE_SENSOR: '00,10',
+                CNTI_NAME: '2222222',
+                CNTI_E_NAME: '2222222',
+                CNTI_VERSION: '1.1',
+                CNTI_F_NAME: '2222222',
+                CNTI_F_ID: '2222222',
+                CNTI_F_PATH: '/uploads/contents/XR.10.testtesttest',
+                CNTI_MIN_UCNT: 1,
+                CNTI_MAX_UCNT: 2,
+                CNTI_DESC: '인설트 테스트',
+                CNTI_SORT: 161,
+                CNTI_SORTBY: 'DESC',
+            });
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
 });
+
+// 데이터 업데이트 POST로 해야하나 get으로 임시
+app.get('/api/updateCont/:idx',async (req, res) => {
+        try {
+            const idx = (req.params.idx) ? req.params.idx : 134;
+            const contInfo = await cntntInf.findOne({
+                where: {
+                    CNTI_IDX: idx,
+                },
+            });
+            if (contInfo) {
+                contInfo.CNTI_NAME = '1515151';
+            }
+            await contInfo.commit();
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+});
+
+
+// 데이터 삭제
+app.get('/api/deleteCont/:idx',async (req, res) => {
+
+        try {
+            const idx = (req.params.idx) ? req.params.idx : 134;
+            const contInfo = await cntntInf.findOne({
+                where: {
+                    CNTI_IDX: idx,
+                },
+            });
+            if (contInfo) {
+                await contInfo.destroy();
+            }
+            await contInfo.commit();
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            res.status(500).json({error: 'Internal Server Error'});
+        }
+});
+
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
