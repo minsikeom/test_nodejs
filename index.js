cccc5126b014164f5d7362ca872746a2c19c871d
airpass <cwme12@airpass.co.kr>
2024-02-22 16:49:22 +0900

    aws,모델 시퀄라이즈 작업

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const sequelize = new Sequelize(config.development);
 /// 모델 불러오기
 const cntntInf = require('./models/xr_cntnt_inf');
 const RS = require('./models/xr_cntnt_rs');
+// 뷰 페이지 랜더링
 const path = require('path');
 const ejs = require('ejs');
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//aws 연동 /////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////aws 연동 셋팅 ///////////////////////////////////////////////////////////////
 const { S3Client, GetObjectCommand, ListObjectsV2Command} = require('@aws-sdk/client-s3');
 const { createReadStream, createWriteStream } = require('fs');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
@@ -25,7 +27,7 @@ const bucketName = 'airpass';
 const prefix = 'uploads/contents/AL.10.Demo/Release';
 const key = 'uploads/contents/AL.10.Demo/Release/AL.10.Demo.zip';
 
-// S3에서 파일 목록 조회 함수
+///////////////////////////////S3에서 파일 목록 조회 함수 //////////////////////////////////////////////////////////
 async function listS3Files(bucketName, prefix = '') {
     const listObjectsCommand = new ListObjectsV2Command({
         Bucket: bucketName,
@@ -38,9 +40,9 @@ async function listS3Files(bucketName, prefix = '') {
         if (Contents && Array.isArray(Contents)) {
             // 파일 목록 출력
             Contents.forEach((object) => {
-                console.log('파일 이름:', object.Key);
-                console.log('파일 크기:', object.Size, '바이트');
-                console.log('---');
+                // console.log('파일 이름:', object.Key);
+                // console.log('파일 크기:', object.Size, '바이트');
+                // console.log('---');
             });
 
             return Contents; // 필요에 따라 파일 목록을 반환할 수 있습니다.
@@ -50,9 +52,9 @@ async function listS3Files(bucketName, prefix = '') {
         }
 }
 
-listS3Files(bucketName, prefix)
-    .then((fileList) => console.log('파일 목록 조회 완료:', fileList))
-    .catch((err) => console.error('파일 목록 조회 중 오류:', err));
+//listS3Files(bucketName, prefix)
+//    .then((fileList) => console.log('파일 목록 조회 완료:', fileList))
+//    .catch((err) => console.error('파일 목록 조회 중 오류:', err));
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -75,10 +77,10 @@ async function generatePresignedUrl(bucketName, key) {
         console.error('Error generating pre-signed URL:', error);
     }
 }
-console.log('파일 다운로드 임시 url 발급')
-generatePresignedUrl(bucketName,key);
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// console.log('파일 다운로드 임시 url 발급')
+// // generatePresignedUrl(bucketName,key);
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////API 작업 with 시퀄라이즈(ORM) ///////////////////////////////////////////////////////
 
@@ -97,7 +99,6 @@ app.get('/api/getContList', async (req, res) => {
             ],
         });
         res.json(contList);
-        await sequelize.close();
     } catch (error) {
         console.error('Error fetching users:', error);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -120,15 +121,15 @@ app.get('/api/getContInfo/:idx', async (req, res) => {
                     },
                 ],
             });
-            res.json(contInfo);
-            await sequelize.close();
+           // res.json(contInfo);
+            res.render('index', { data:contInfo.toJSON() });
         } catch (error) {
             console.error('Error fetching users:', error);
             res.status(500).json({error: 'Internal Server Error'});
         }
 });
 
-// 콘텐츠 저장 POST 방식으로 데이터 받아서 해야하나 임시로
+// 콘텐츠 저장 POST 방식으로 데이터 받아서 해야하나 임시로 get
 app.get('/api/insertCont',async (req, res) => {
         try {
             // 테스트 내역 저장
@@ -151,13 +152,15 @@ app.get('/api/insertCont',async (req, res) => {
                 CNTI_SORT: 161,
                 CNTI_SORTBY: 'DESC',
             });
+            await newCont.commit();
         } catch (error) {
+            await newCont.rollback();
             console.error('Error fetching users:', error);
             res.status(500).json({error: 'Internal Server Error'});
         }
 });
 
-// 데이터 업데이트 POST로 해야하나 get으로 임시
+// 데이터 업데이트 POST 로 해야하나 get 으로 임시
 app.get('/api/updateCont/:idx',async (req, res) => {
         try {
             const idx = (req.params.idx) ? req.params.idx : 134;
@@ -168,18 +171,19 @@ app.get('/api/updateCont/:idx',async (req, res) => {
             });
             if (contInfo) {
                 contInfo.CNTI_NAME = '1515151';
+                await contInfo.save();
             }
             await contInfo.commit();
         } catch (error) {
+            await contInfo.rollback();
             console.error('Error fetching users:', error);
             res.status(500).json({error: 'Internal Server Error'});
         }
 });
 
 
-// 데이터 삭제
+// 데이터 삭제 post로 해야하나 get 으로 임시
 app.get('/api/deleteCont/:idx',async (req, res) => {
-
         try {
             const idx = (req.params.idx) ? req.params.idx : 134;
             const contInfo = await cntntInf.findOne({
@@ -192,15 +196,28 @@ app.get('/api/deleteCont/:idx',async (req, res) => {
             }
             await contInfo.commit();
         } catch (error) {
+            await contInfo.rollback();
             console.error('Error fetching users:', error);
             res.status(500).json({error: 'Internal Server Error'});
         }
+
+});
+
+app.post('/submit', (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const nickName = req.body.nickName;
+    // 여기서 데이터를 저장하거나 다른 처리를 수행할 수 있습니다.
+    // get 방식 req.query.{param}  url에서 변수를 받아서 치환은 요렇게
+    // post 방식 req.body.{param}  변수 치환은 이렇게
+    console.log('사용자 이름:', username);
+    console.log('이메일:', email);
+    // 다음 페이지로 리다이렉트 또는 응답을 보낼 수 있습니다.
+    res.send(username+'//'+email+'//'+nickName+'폼 데이터가 성공적으로 전송되었습니다.');
 });
 
 
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 
 ///// 뷰 페이지 렌더링 /////////////////////////////////////////////////////////////////////////////////////
@@ -213,6 +230,7 @@ app.get('/page', (req, res) => {
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 // 포트 연결 확인
